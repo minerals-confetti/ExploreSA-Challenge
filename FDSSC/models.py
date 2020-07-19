e2c23b6161bc714d37e450da252f43940136bff2
epicrunze <runze.zhang2001@gmail.com>
2020-07-19 10:59:13 -0400

    created revised fasterDSSC

--- FDSSC/models.py
+++ FDSSC/models.py
@@ -116,4 +116,78 @@ class FDSSC_model(nn.Module):
         return output
 
     
-    
+class FerDSSC_model(nn.Module):
+
+    def __init__(self, input_shape, classes):
+        '''Input shape in the form of (N,C,H,W,D)'''
+        super(FerDSSC_model, self).__init__()
+        self.input_shape = input_shape
+
+        global growth_rate
+        growth_rate = 12
+
+        # add padding since original paper would skip over channels that don't divide evenly into the convolution
+        depth_after_spec = input_shape[3]
+        # definitions for dense spectral layers
+        self.input_spec_conv = nn.Conv3d(1, 24, kernel_size=(1, 1, 7), stride=(1, 1, 1), padding=(0, 0, 3))
+        self.spectral_conv1 = Spectral_conv(24)
+        self.spectral_conv2 = Spectral_conv(36)
+        self.spectral_conv3 = Spectral_conv(48)
+        self.bn_prelu1 = Bn_prelu(60)
+
+        #reshaping and transforming to prep for spatial conv
+        hidden_states = 30
+        conv_depth = depth_after_spec # maybe consider setting this to an integer so we can apply transfer learning? (need to pad as well)
+        self.reshape_conv = nn.Conv3d(60, hidden_states, kernel_size=(1, 1, conv_depth), stride=(1, 1, 1))
+        self.bn_prelu2 = Bn_prelu(hidden_states)
+
+        # definitions for dense spatial layers
+        self.input_spat_conv = nn.Conv3d(1, 24, kernel_size=(3, 3, hidden_states), stride=(1, 1, 1))
+        self.spatial_conv1 = Spatial_conv(24)
+        self.spatial_conv2 = Spatial_conv(36)
+        self.spatial_conv3 = Spatial_conv(48)
+        self.bn_prelu3 = Bn_prelu(60)
+
+        # pooling for classification
+        self.pool1 = nn.AvgPool3d(kernel_size=(7, 7, 1), stride=(1, 1, 1))
+
+        self.drop1 = nn.Dropout(0.5)
+
+        self.fc1 = nn.Linear(in_features=60, out_features=classes)
+        self.softm = nn.Softmax()
+
+    def forward(self, x):
+        # spectral conv layers
+        x_spec0 = self.input_spec_conv(x)
+        x_spec1 = self.spectral_conv1(x_spec0)
+        x_spec1_ = torch.cat((x_spec0, x_spec1), dim=1)
+        x_spec2 = self.spectral_conv2(x_spec1_)
+        x_spec2_ = torch.cat((x_spec0, x_spec1, x_spec2), dim=1)
+        x_spec3 = self.spectral_conv3(x_spec2_)
+        xspec = torch.cat((x_spec0, x_spec1, x_spec2, x_spec3), dim=1)
+        xspec = self.bn_prelu1(xspec)
+
+        # transforming dims
+        trans1 = self.reshape_conv(xspec)
+        trans1 = self.bn_prelu2(trans1)
+        trans1 = trans1.permute(0, 4, 2, 3, 1) # moving channels axis to the depth axis
+        print(trans1.shape)
+
+        # spatial conv layers
+        x_spat0 = self.input_spat_conv(trans1)
+        x_spat1 = self.spatial_conv1(x_spat0)
+        x_spat1_ = torch.cat((x_spat0, x_spat1), dim=1)
+        x_spat2 = self.spatial_conv2(x_spat1_)
+        x_spat2_ = torch.cat((x_spat0, x_spat1, x_spat2), dim=1)
+        x_spat3 = self.spatial_conv3(x_spat2_)
+        xspat = torch.cat((x_spat0, x_spat1, x_spat2, x_spat3), dim=1)
+        xspat = self.bn_prelu3(xspat)
+
+        pooled = self.pool1(xspat)
+        flattened = torch.flatten(pooled, start_dim=1)
+        dropped = self.drop1(flattened)
+        logits = self.fc1(dropped)
+        
+        output = self.softm(logits)
+
+        return output
